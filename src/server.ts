81d878e469aa45a82c19c91008cad5e055102faa
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-10-26 15:08:38 +0200

    Implement delete single user functionality

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,30 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+const users = ['Alex', 'Bailey', 'Fynn', 'Kiwi'];
+
+app.delete('/api/users/:name', (request, response) => {
+  const name = request.params.name;
+  const isKnown = users.includes(name);
+  if (isKnown) {
+    users.splice(users.indexOf(name), 1);
+    response.send(`${name} is deleted`);
+  } else {
+    response.status(404).send('Name is unknown');
+  }
+});
+
 app.get('/api/users/:name', (request, response) => {
-  response.send(request.params.name);
+  const isNameKnow = users.includes(request.params.name);
+  if (isNameKnow) {
+    response.send(request.params.name);
+  } else {
+    response.status(404).send('Name is unknown');
+  }
+  // if name is unknown, return 404 with "Name is unknown"
 });
 
 app.get('/api/users', (_request, response) => {
-  const users = ['Alex', 'Bailey', 'Fynn', 'Kiwi'];
-
   response.send(users);
 });
 
